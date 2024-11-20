0eff9880c0ff8e50aad938a28edb76e2c4f92c32
RobBEN94 <robnavar84@gmail.com>
2024-11-20 00:46:15 -0600

    Added byte transmission, error correction and noisy network config

--- application.py
+++ application.py
@@ -7,8 +7,10 @@ from netqasm.sdk.epr_socket import EPRSocket
 
 from squidasm.util.routines import create_ghz
 
+import netsquid as ns
+
 class AnonymousTransmissionProgram(Program):
-    def __init__(self, node_name: str, node_names: list, send_bit: bool = None):
+    def __init__(self, node_name: str, node_names: list, send_byte: int = None, apply_corr: bool = False):
         """
         Initializes the AnonymousTransmissionProgram.
 
@@ -17,7 +19,8 @@ class AnonymousTransmissionProgram(Program):
         :param send_bit: The bit to be transmitted; set to None for nodes that are not the sender.
         """
         self.node_name = node_name
-        self.send_bit = send_bit
+        self.send_byte = send_byte
+        self.apply_corr = apply_corr
 
         # Find what nodes are next and prev based on the node_names list
         node_index = node_names.index(node_name)
@@ -51,71 +54,139 @@ class AnonymousTransmissionProgram(Program):
         # Initialize next and prev sockets using the provided context
         self.setup_next_and_prev_sockets(context)
         
-        for _ in range(3):
-            self.qubit = yield from self.gen_ghz(context)
+        recv_byte = []
         
-            # Run the anonymous transmission protocol and retrieve the received bit
-            received_bit = yield from self.anonymous_transmit_bit(context, self.send_bit, 1)
+        if self.send_byte is not None:
 
-            print(f"{self.node_name} has received the bit: {received_bit}")
+            binary_string = str(self.send_byte)
+
+            # Check if the input is a valid byte (8 bits)
+            if len(binary_string) != 8 or not all(bit in "01" for bit in binary_string):
+                raise ValueError("Input must be an 8-bit binary number (e.g., 10101011)")
+
+            # Iterate through each bit
+            for bit in binary_string:
+                
+                # Convert bit (char) to integer for comparison
+                bit = int(bit)
+                
+                # Run the anonymous transmission protocol and retrieve the received bit
+                received_bit = yield from self.anonymous_transmit_bit(context, bit, self.apply_corr)
+                
+                recv_byte.append(received_bit)
+                    
+        else:
             
-        for _ in range(3):
-            self.qubit = yield from self.gen_ghz(context)
+            # Iterate through each bit
+            for _ in range(8):
+                
+                # Run the anonymous transmission protocol and retrieve the received bit
+                received_bit = yield from self.anonymous_transmit_bit(context)
+                
+                recv_byte.append(received_bit)
         
-            # Run the anonymous transmission protocol and retrieve the received bit
-            received_bit = yield from self.anonymous_transmit_bit(context, self.send_bit, 0)
+        # Convert the list to a string
+        recv_byte_string = ''.join(map(str,recv_byte))
 
-            print(f"{self.node_name} has received the bit: {received_bit}")
-            
-        for _ in range(3):
-            self.qubit = yield from self.gen_ghz(context)
+        print(f"{self.node_name} has received the byte: {recv_byte_string}")
         
-            # Run the anonymous transmission protocol and retrieve the received bit
-            received_bit = yield from self.anonymous_transmit_bit(context, self.send_bit, 1)
+        run_time = ns.sim_time()
+        
+        return {"name": self.node_name, "run_time": run_time, "recvd_byte": recv_byte_string}
 
-            print(f"{self.node_name} has received the bit: {received_bit}")
-            
-        return {}
-
-    def anonymous_transmit_bit(self, context: ProgramContext, send_bit: bool = None, bit: int = None) -> Generator[None, None, bool]:
+    def anonymous_transmit_bit(self, context: ProgramContext, send_bit: bool = None, correction: bool = False) -> Generator[None, None, bool]:
         """
         Anonymously transmits a bit to other nodes in the network as part of the protocol.
 
         :param context: The program's execution context.
         :param send_bit: Bit to be sent by the sender node; receivers should leave this as None.
+        :param correction: If True applies repetition code of length three.
         :return: The bit received through the protocol, or the sent bit if this node is the sender.
         """
         
-        if send_bit is True:
-            if bit == 1:
-                self.qubit.Z()
+        if correction:
+            
+            encoded_recv_bit=[]
+            
+            for _ in range(3):
+                
+                # Save GHZ qubit at current node
+                self.qubit = yield from self.gen_ghz(context) 
+                
+                if send_bit == 1:
+                    self.qubit.Z()
+                    yield from context.connection.flush()
+                
+                self.qubit.H()
+                q_measure = self.qubit.measure()
                 
                 yield from context.connection.flush()
+                        
+                message = f"{q_measure}"
                 
-        self.qubit.H()
-        q_measure = self.qubit.measure()
-        
-        yield from context.connection.flush()
+                self.broadcast_message(context,message)
                 
-        message = f"{q_measure}"
-        
-        self.broadcast_message(context,message)
-        
-        recv_message = yield from self.receive_message(context)
-        
-        # Add current node's result
-        recv_message.append(int(message))
-        
-        # Check measurements parity
-        is_odd = sum(recv_message) % 2 != 0
+                # Recieve classical message from all nodes
+                recv_message = yield from self.receive_message(context)
+                
+                # Add current node's result
+                recv_message.append(int(message))
+                
+                # Check measurements parity
+                is_odd = sum(recv_message) % 2 != 0
 
-        # Asign bit value based on parity
-        if is_odd:
-            recv_bit = 1
-        else:
-            recv_bit = 0
+                # Asign bit value based on parity
+                if is_odd:
+                    recv_bit = 1
+                else:
+                    recv_bit = 0
+                
+                encoded_recv_bit.append(recv_bit)
+                
+            ones = encoded_recv_bit.count(1)
+            zeros = encoded_recv_bit.count(0)
             
-        return recv_bit
+            # Use majority voting to decode
+            if ones > zeros:
+                corrected_bit = 1
+            else:
+                corrected_bit = 0
+            
+            return corrected_bit
+        
+        else:
+            # Save GHZ qubit at current node
+            self.qubit = yield from self.gen_ghz(context)
+            
+            if send_bit == 1:
+                self.qubit.Z()
+                yield from context.connection.flush()
+                    
+            self.qubit.H()
+            q_measure = self.qubit.measure()
+            
+            yield from context.connection.flush()
+                    
+            message = f"{q_measure}"
+            
+            self.broadcast_message(context,message)
+            
+            # Recieve classical message from all nodes
+            recv_message = yield from self.receive_message(context)
+            
+            # Add current node's result
+            recv_message.append(int(message))
+            
+            # Check measurements parity
+            is_odd = sum(recv_message) % 2 != 0
+
+            # Asign bit value based on parity
+            if is_odd:
+                recv_bit = 1
+            else:
+                recv_bit = 0
+            
+            return recv_bit
 
     def broadcast_message(self, context: ProgramContext, message: str):
         """Broadcasts a message to all nodes in the network."""
@@ -143,7 +214,7 @@ class AnonymousTransmissionProgram(Program):
             self.prev_epr_socket = context.epr_sockets[self.prev_node_name]
             
     def gen_ghz(self, context: ProgramContext):
-                
+        """Generates GHZ state on the whole network."""
         qubit, m = yield from create_ghz(
             context.connection,
             self.prev_epr_socket,
--- run_simulation.py
+++ run_simulation.py
@@ -8,12 +8,20 @@ nodes = ["Alice", "Bob", "Charlie", "David"]
 # import network configuration from file
 cfg = StackNetworkConfig.from_file("config.yaml")
 
+byte = 11111111
+
 # Create instances of programs to run
-alice_program = AnonymousTransmissionProgram(node_name="Alice", node_names=nodes, send_bit=True)
-bob_program = AnonymousTransmissionProgram(node_name="Bob", node_names=nodes)
+alice_program = AnonymousTransmissionProgram(node_name="Alice", node_names=nodes)
+bob_program = AnonymousTransmissionProgram(node_name="Bob", node_names=nodes,send_byte=byte, apply_corr=True)
 charlie_program = AnonymousTransmissionProgram(node_name="Charlie", node_names=nodes)
 david_program = AnonymousTransmissionProgram(node_name="David", node_names=nodes)
 
 # Run the simulation. Programs argument is a mapping of network node labels to programs to run on that node
-run(config=cfg, programs={"Alice": alice_program, "Bob": bob_program,
-                          "Charlie": charlie_program, "David": david_program}, num_times=1)
+results = run(config=cfg, programs={"Alice": alice_program, "Bob": bob_program,
+                          "Charlie": charlie_program, "David": david_program}, num_times=3)
+
+print(results)
+
+
+# Calculate and print Average Success Probability and Average Transmission Speed.
+# Hamming distance for success probability
